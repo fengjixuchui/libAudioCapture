6d13b7bebcce71ce5307b4318a08a9a16aab5cc2
Jim Huang <cplusplus.tw@gmail.com>
2019-10-26 00:27:38 +0800

    Add an example for demonstration
    	new file:   examples/libAudioCapture-demo.pro
    	new file:   examples/main.cpp

---
+++ examples/main.cpp
@@ -0,0 +1,59 @@
+#include <cassert>
+#include <atomic>
+#include <fstream>
+#include <iostream>
+#include <thread>
+
+#include "AudioCaptureEngine.h"
+
+int main()
+{
+    AudioCaptureEngine ENGINE;
+    ENGINE.Init();
+
+    std::atomic<bool> bStop(false);
+    std::thread t = std::thread([&](){
+        std::cout << "recording...";
+        for (auto i=0; i<5; i++) {
+            std::cout << "\r" << "countdown " << i+1 << std::flush;
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+        std::cout << std::endl;
+        bStop.store(true);
+    });
+
+    uint32_t const PERIOD = 1024;   // 1024 frames per period
+    uint32_t const CHANNEL = 2;     // stereo
+    uint32_t const bufferSize = 2 * CHANNEL * PERIOD; // 2 means 16 bits (2 bytes)
+    char* buffer = new char [bufferSize];
+
+    std::ofstream output("./sound.pcm", std::ios::binary | std::ios::trunc);
+
+    while (!bStop) {
+        long ret = -1;
+        ret = ENGINE.GetBuffer(buffer, PERIOD);
+        if (ret < 0) {
+            std::clog << "XRUN...\n";
+            continue;
+        }
+
+        if (output.is_open()) {
+            output.write(buffer, bufferSize);
+            assert(output.good());
+        }
+    }
+
+    if (output.is_open()) {
+        output.close();
+    }
+
+    if (buffer) {
+        delete [] buffer;
+    }
+
+    if (t.joinable()) {
+        t.join();
+    }
+
+    ENGINE.Uninit();
+}
